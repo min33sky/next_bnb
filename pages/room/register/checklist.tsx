da6b00036714b6f62a422627ed49aa15ef786179
min33sky <min33sky@naver.com>
2021-03-07 15:34:15 +0900

    Add: checklist - 1

---
+++ pages/room/register/checklist.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RegisterRoomChecklist from '../../../components/room/register/RegisterRoomChecklist';
+
+/**
+ * 체크 리스트 페이지
+ * /room/register/checklist
+ */
+export default function checklist() {
+  return <RegisterRoomChecklist />;
+}
