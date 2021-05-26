96e438f891aebbb887d529c6bede3ef931f44443
min33sky <min33sky@naver.com>
2021-05-26 19:50:33 +0900

    asdasdasd

---
+++ components/Home/SearchRoomBar/SearchRoomCheckOutDate.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from 'styles/palette';
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 70px;
+  border: 2px solid transparent;
+  border-radius: 12px;
+  cursor: pointer;
+  &:hover {
+    border-color: ${palette.gray_dd};
+  }
+`;
+
+export default function SearchRoomCheckOutDate() {
+  return <Container>체크아웃</Container>;
+}
--- components/Home/SearchRoomBar/index.tsx
+++ components/Home/SearchRoomBar/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import palette from 'styles/palette';
+import SearchRoomBarLocation from './SearchRoomBarLocation';
+import SearchRoomCheckInDate from './SearchRoomCheckInDate';
+import SearchRoomCheckOutDate from './SearchRoomCheckOutDate';
+import SearchRoomGuests from './SearchRoomGuests';
 
 const Container = styled.div`
   width: 100%;
@@ -8,16 +13,36 @@ const Container = styled.div`
   align-items: center;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.08);
   border-radius: 12px;
+
+  .search-room-bar-inputs {
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+    .search-room-bar-input-divider {
+      width: 1px;
+      height: 44px;
+      background-color: ${palette.gray_dd};
+    }
+  }
 `;
 
 /**
- * 검색 바
+ * 숙소 검색 바
  * @returns Search Input
  */
 export default function SearchRoomBar() {
   return (
     <Container>
-      <div className="search-room-bar-inputs" />
+      <div className="search-room-bar-inputs">
+        <SearchRoomBarLocation />
+        <div className="search-room-bar-input-divider" />
+        <SearchRoomCheckInDate />
+        <div className="search-room-bar-input-divider" />
+        <SearchRoomCheckOutDate />
+        <div className="search-room-bar-input-divider" />
+        <SearchRoomGuests />
+      </div>
     </Container>
   );
 }
